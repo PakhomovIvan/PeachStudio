67a19d66b87d91adf884e22e7d526a72b3699062
Ivan Pakhomov <ip1133@ya.ru>
2025-03-24 02:34:08 +0300

    feat: add active item menu

--- src/scripts/modules/menu.js
+++ src/scripts/modules/menu.js
@@ -1,10 +1,22 @@
 export const menuSlider = () => {
-  const menuNav = document.querySelector('.navigate__offices')
+  const menuNavLogo = document.querySelector('.navigate__offices')
   const listNav = document.querySelector('.navigate__list')
   const listNavArrow = document.querySelector('.navigate__offices-arrow')
 
-  menuNav.addEventListener('click', () => {
+  const testNav = document.querySelectorAll('#nav-menu')
+  const testTestNav = document.querySelector('#testtest')
+
+  menuNavLogo.addEventListener('click', () => {
     listNav.classList.toggle('show')
     listNavArrow.classList.toggle('rotate')
+  })
+
+  testTestNav.addEventListener('click', (e) => {
+    if (e.target.id === 'nav-menu') {
+      testNav.forEach((e) => {
+        e.classList.remove('active')
+      })
+      e.target.classList.add('active')
+    }
   })
 }
