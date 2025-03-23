5454cf484a46af9fa35c14a291562b883d464cba
Ivan Pakhomov <ip1133@ya.ru>
2025-03-23 23:59:54 +0300

    ref: paginator decomposition js, add image

---
+++ src/scripts/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.mySwiper', {
+  effect: 'coverflow',
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+  },
+  pagination: {
+    el: '.paginator',
+    clickable: true,
+  },
+  keyboard: true,
+})
