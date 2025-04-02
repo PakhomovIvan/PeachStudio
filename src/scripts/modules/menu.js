fca2fe210334d278cfe366379ac747b46b65cb34
Ivan Pakhomov <ip1133@ya.ru>
2025-04-02 23:16:34 +0300

    feat: added transparency of the substrate

--- src/scripts/modules/menu.js
+++ src/scripts/modules/menu.js
@@ -2,25 +2,25 @@ export const menuSlider = () => {
   const menuNavLogo = document.querySelector('.navigate__offices')
   const listNav = document.querySelector('.navigate__list')
   const listNavArrow = document.querySelector('.navigate__offices-arrow')
-  const navigateMap = document.querySelector('.navigate__map')
+  const navMap = document.querySelector('.navigate__map')
 
   const testNav = document.querySelectorAll('.nav-menu')
-  const testTestNav = document.querySelector('#nav-menu-item')
+  const navMenu = document.querySelector('#nav-menu-item')
 
   menuNavLogo.addEventListener('click', () => {
     listNav.classList.toggle('show')
     listNavArrow.classList.toggle('rotate')
+    navMenu.classList.toggle('blur')
+    navMap.classList.toggle('blur')
   })
 
-  testTestNav.addEventListener('click', (e) => {
+  navMenu.addEventListener('click', (e) => {
     if (e.target.className === 'nav-menu') {
       testNav.forEach((e) => {
         e.classList.remove('active')
       })
       e.target.classList.add('active')
-
-      navigateMap.innerHTML = `<img src="./img/images/map/${e.target.id}.svg" alt="offices-map" />`
+      navMap.innerHTML = `<img src="./img/images/map/${e.target.id}.svg" alt="offices-map" />`
     }
-    console.log(e)
   })
 }
